a6c1dfa0585786cee60e5e9d5d4c5f963c966813
skookumchoocher <natebrooks004@gmail.com>
2026-04-20 18:12:13 -0500

    fix: increase csp 2

--- web/internal/server/server.go
+++ web/internal/server/server.go
@@ -163,7 +163,7 @@ func withSecurityHeaders(next http.Handler) http.Handler {
 				"connect-src 'self' https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://ep1.adtrafficquality.google https://cloudflareinsights.com; "+
 				"img-src 'self' data: https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://tpc.googlesyndication.com; "+
 				"style-src 'self' 'unsafe-inline'; "+
-				"script-src 'self' https://pagead2.googlesyndication.com https://static.cloudflareinsights.com; "+
+				"script-src 'self' https://pagead2.googlesyndication.com https://static.cloudflareinsights.com https://ep2.adtrafficquality.google; "+
 				"frame-src https://googleads.g.doubleclick.net https://tpc.googlesyndication.com; "+
 				"object-src 'none'; base-uri 'self'; frame-ancestors 'none';",
 		)
